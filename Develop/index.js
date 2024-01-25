d4bb070ff00b28f3da72e52bcf2e16f552098b9b
Harry P <harryepotter@outlook.com>
2024-01-25 13:21:40 +1100

    Improved structure

--- Develop/index.js
+++ Develop/index.js
@@ -63,7 +63,9 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then((answers) => {
+    inquirer
+    .prompt(questions)
+    .then((answers) => {
         writeToFile("readme.md", generateMarkdown(answers));
     });
 }
