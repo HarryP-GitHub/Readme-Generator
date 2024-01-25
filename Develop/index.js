60aa831e7f303e76a93d18c51dcc02edb3e0ddfa
Harry P <harryepotter@outlook.com>
2024-01-25 13:24:09 +1100

    Saving javascript

--- Develop/index.js
+++ Develop/index.js
@@ -64,8 +64,8 @@ function writeToFile(fileName, data) {
 // TODO: Create a function to initialize app
 function init() {
     inquirer
-    .prompt(questions)
-    .then((answers) => {
+      .prompt(questions)
+      .then((answers) => {
         writeToFile("readme.md", generateMarkdown(answers));
     });
 }
